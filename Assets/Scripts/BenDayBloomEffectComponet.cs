729081f4001dcde049b1748ab184278e6d694220
Daniel Pascual López <uo269728@uniovi.es>
2024-07-26 00:23:58 +0200

    Parametros

--- Assets/Scripts/BenDayBloomEffectComponet.cs
+++ Assets/Scripts/BenDayBloomEffectComponet.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using Vector2 = UnityEngine.Vector2;
 
 [VolumeComponentMenuForRenderPipeline("Custom/Ben Day Bloom",typeof(UniversalRenderPipeline) )]
 public class BenDayBloomEffectComponet : VolumeComponent, IPostProcessComponent
 {
-    // Start is called before the first frame update
+    [Header("Bloom settings")]
+    public FloatParameter tresHold = new FloatParameter(0.9f,true);
+
+    public FloatParameter intensity = new FloatParameter(1.0f, true);
+
+    public ClampedFloatParameter scatter = new ClampedFloatParameter(0.7f, 0.0f, 1.0f, true);
+
+    public IntParameter clamp = new IntParameter(65472, true);
+
+    public ClampedIntParameter maxIterations = new ClampedIntParameter(6,0,10);
+
+    public NoInterpColorParameter tint = new NoInterpColorParameter(Color.white);
+
+    [Header("Benday")]
+    public IntParameter dotsDensity = new IntParameter(10,true);
+
+    public ClampedFloatParameter dotsCutOff = new ClampedFloatParameter(0.4f,0.0f,1.0f,true);
+    public Vector2Parameter scrollDirection = new Vector2Parameter(new Vector2());
+
     public bool IsActive()
     {
         return true;
     }
+
     public bool IsTileCompatible()
     {
-        return false;
+       return false;
     }
 }
